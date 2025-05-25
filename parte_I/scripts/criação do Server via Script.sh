c233659141268400c60d7c8e2784d0971ed07c1d
Diego Alves Moreira <rm552603@fiap.com.br>
2025-05-24 21:53:37 -0300

    Add temporary document for SIGHTHUNTERS ETL process

---
+++ parte_I/scripts/criação do Server via Script.sh
@@ -0,0 +1,12 @@
+-- Cria o SGBD Azure SQL chamado sqlserver-kidelicia  
+az sql server create -l brazilsouth -g cp3-dw -n kidelicia -u admsql -p db@FIAP25 --enable-public-network true
+
+-- Cria o banco de dados chamado dburgerdbstage0  
+az sql db create -g cp3-dw -s kidelicia -n dburgerdbstage0 --service-objective Basic --backup-storage-redundancy Local --zone-redundant false
+
+-- Libera o acesso para qualquer endereço IP da rede (somente para testes iniciais)  
+az sql server firewall-rule create -g cp3-dw -s kidelicia -n AllowAll --start-ip-address 0.0.0.0 --end-ip-address 255.255.255.255
+
+-- Usuário e senha do banco de dados  
+-- admsql
+-- db@FIAP25
